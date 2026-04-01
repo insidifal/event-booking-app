f0c19df84d34d428604e9e2f47f10d40fbd04791
insidifal <joshkenmarks@gmail.com>
2026-04-02 00:47:15 +0200

    database vars

--- app/database.py
+++ app/database.py
@@ -9,16 +9,14 @@ logger = logging.getLogger(__name__)
 pool = None
 
 async def get_database_pool():
-    host = os.getenv("DB_HOST", "localhost")
-    password = os.getenv("MYSQL_PW")
     global pool
     if pool is None:
         pool = await aiomysql.create_pool(
-            host=host,
-            port=3306,
-            user="root",
-            password=password,
-            db="events_booking_app",
+            host=os.getenv("DB_HOST", "localhost"),
+            port=os.getenv("DB_PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("MYSQL_PW"),
+            db=os.getenv("DATABASE"),
             autocommit=True,
             cursorclass=aiomysql.DictCursor,
             client_flag=CLIENT.FOUND_ROWS
